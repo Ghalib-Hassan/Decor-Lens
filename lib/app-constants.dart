4c6dd9474ed7273f6da527bebd70b2d40a93deb2
Ghalib hassan <ghalibthassan@gmail.com>
2025-07-12 14:56:32 +0500

    Google Maps integrated

---
+++ lib/app-constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  final String googleMapKey = 'AIzaSyA8EIJNaB2Yo5c94phE1EODBaasXfZ9KLM';
+
+  // static String stripePublishableKey =
+  //     "pk_test_51QkUcYGHGGXzF3qmH4RgyQkREyDMjcs79D8NTd8u0OBPFecL50wyWcS6l5rjctquzBzZxjyBh7mlWH5afanPfWjR00SCnNvuYe";
+  // static String stripeSecretKey =
+  //     "sk_test_51QkUcYGHGGXzF3qmvkSh5co5SJWFukW3PQoWI96npLCK1cpA50CLIQF8FTnfqvPGds9Ee8Dtwh8u6VavjEJB6nxR00b4e3vGuB";
+}
