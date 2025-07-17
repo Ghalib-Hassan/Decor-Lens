cc12a69975f4c29fd4a9e8a9d708496415991c61
Ghalib hassan <ghalibthassan@gmail.com>
2025-07-17 17:59:10 +0500

    Remove app_constants.dart containing Stripe keys

--- lib/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  final String googleMapKey = 'AIzaSyA8EIJNaB2Yo5c94phE1EODBaasXfZ9KLM';
-  static String stripePublishableKey =
-      "pk_test_51RlU81B6PcP0GjlqN6L5HAN8nGj5BN5K2L2ujMFhfi18RwM79zvIMh9g6SLH0GyKCrPcGHIMQq1jlB2OXRRhuSg100qthkYhGn";
-  static String stripeSecretKey =
-      "sk_test_51RlU81B6PcP0GjlqAeYS4tBrSpfTxA8CTG5FIlby2Lrrf0iOEP6bSrgmi8LSyFLf8yQ57ziicdytDyZML7zn7N7S00pWljt94D";
-}
